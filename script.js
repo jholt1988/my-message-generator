4b5f665722bd7c6d11233a9b00b83fa7e4d5ba80
Jordan A Holt <jordanh316@gmail.com>
2021-04-21 04:28:05 -0500

    Create Cookie object that holds the pieces of the the output message. Also started to change program from a fortune cookie program to a cards against humanity.

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log('Jordan A hOLT')
+
+const cookie = {
+
+      begin : [],
+      middle : [],
+       end : [],
+      message : message,
+
+    get begin (){
+      return this._begin;
+    },
+
+    get middle(){
+      return this._middle;
+    },
+
+    get end(){
+      return this._end;
+      },
+
+       begin = [
+                ['The fortune you seek', 'A closed mouth', 'A conclusion'],
+                ['A foolish man', 'A wise man listens to', 'Hearing your Neighbor having loud sex' ],
+                ['Iphone dick pics', 'The Shaft', 'Cultural appropriation']
+       ];,
+
+         middle =[
+           ['Pussy Wagon', 'Hitler\'s new haircut','Betting all your money on Cedrick Diggory to Win the Triwizard Tournament'],
+          ['The Goblet of Fire', 'Making Date rape in Potions class','Peter Pettigrews Penis' ],
+           ['The Weasley\'s entire Net Worth' , 'Pretending not to Notice They changed Dumbledore in the 3rd movie', 'A ThreeSome With The Weasley Twins']
+         ];,
+
+         end = [
+           ['An unsolicited dick pic from Harry Potter', 'Racist comments that Don Cherry Makes Nazis Pearl Harbor', 'Joe Biden'],
+           ['Why does my dick burn?', 'In Mexico, you can do __________ while you are __________.', 'After masturbating I like to do __________.'] ,
+           ['Trump will solve all of Americas problems by __,', 'Trump will solve all of Americas problems by __,', 'Trump will solve all of Americas problems by __,' ];,
+         ];,
+       }
+
+
+
+    }
+  }
